591bc55a4e3c9356dd1dde61c20de58fea423c43
Yauhen Kharuzhy <jekhor@gmail.com>
2011-05-02 18:45:18 +0300

    Add CRUD support
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- api/0.2/getBugs.rb
+++ api/0.2/getBugs.rb
@@ -7,46 +7,113 @@ require "json/ext"
 
 require "./db_config.rb"
 
-cgi = CGI.new
-params = cgi.params
 
-bbox = nil
-bbox = params['bbox'][0].split(',').map {|v| v.to_f} if params['bbox'][0]
+class BugsController
+  def initialize
+    @cgi = CGI.new
+    @params = @cgi.params
 
-db = Mysql.connect($dbhost, $dbname, $dbuser, $dbpassword)
-db.options(Mysql::SET_CHARSET_NAME, "utf8")
+    @db = Mysql.connect($dbhost, $dbuser, $dbpassword, $dbname)
+    @db.options(Mysql::SET_CHARSET_NAME, "utf8")
 
-db.query("SET CHARSET utf8")
-db.query("SET NAMES utf8")
-if bbox
-  res = db.query("SELECT * from bugs WHERE lon >= #{bbox[0]} AND lon <= #{bbox[2]} AND lat >= #{bbox[1]} AND lat <= #{bbox[3]} ORDER BY subtype")
-else
-  res = db.query("SELECT * from bugs ORDER BY subtype")
-end
+    @db.query("SET CHARSET utf8")
+    @db.query("SET NAMES utf8")
+  end
 
-j = Hash.new
-j['type'] = 'FeatureCollection'
-j['features'] = Array.new
+  def dispatch!
+    case @cgi.request_method
+    when 'GET':
+      get_features
+    when 'POST':
+      create_feature
+    when 'PUT':
+      update_feature
+    else get_features
+    end
+  end
 
-ja = j['features']
+  def get_features
+    bbox = nil
+    bbox = @params['bbox'][0].split(',').map {|v| v.to_f} if @params['bbox'][0]
 
-res.each_hash {|row|
-  f = {
-    'type' => 'Feature',
-    'geometry' => {
-    'type' => 'Point',
-    'coordinates' => [row['lon'], row['lat']]
-    },
-    'properties' => {
+    if bbox
+      res = @db.query("SELECT * from bugs WHERE lon >= #{bbox[0]} AND lon <= #{bbox[2]} AND lat >= #{bbox[1]} AND lat <= #{bbox[3]} ORDER BY subtype")
+    else
+      res = @db.query("SELECT * from bugs ORDER BY subtype")
+    end
+
+    j = Hash.new
+    j['type'] = 'FeatureCollection'
+    j['features'] = Array.new
+
+    ja = j['features']
+
+    res.each_hash {|row|
+      f = {
+        'type' => 'Feature',
         'id' => row['id'],
+        'geometry' => {
+        'type' => 'Point',
+        'coordinates' => [row['lon'], row['lat']],
+      },
+      'properties' => {
         'subtype' => row['subtype'],
         'type' => row['type'],
         'comments' => row['text'].split(/<hr \/>/).map {|comment| comment.gsub('&quot;', "\"").gsub('&lt;', "<").gsub('&gt;', ">").gsub('&amp;', "&")},
       }
-  }
-  ja << f
-}
+      }
+      ja << f
+    }
 
-cgi.out {
-  JSON.generate(j)
-}
+    @cgi.out {
+      JSON.generate(j)
+    }
+  end
+
+  def create_feature
+    result = Hash.new
+    result['type'] = 'FeatureCollection'
+    res_features = result['features'] = Array.new
+
+    f = nil
+    j = JSON.parse(@params.keys[0])
+    j['features'].each {|feature|
+      subtype = feature['properties']['subtype']
+      subtype ||= 'kerb'
+
+      text = feature['properties']['comments'].join('<hr />') if feature['properties']['comments'].kind_of?(Array)
+      text ||= ''
+
+      g = feature['geometry']
+      @db.query("INSERT INTO bugs (lon, lat, type, subtype, text) VALUES(#{g['coordinates'][0]}, #{g['coordinates'][1]}, 0, '#{subtype}', '#{text}')")
+      id = @db.insert_id
+      res = @db.query("SELECT * FROM bugs WHERE id=#{id}")
+
+      res.each_hash {|row|
+        f = {
+          'type' => 'Feature',
+          'id' => row['id'],
+          'geometry' => {
+          'type' => 'Point',
+          'coordinates' => [row['lon'], row['lat']],
+        },
+        'properties' => {
+          'subtype' => row['subtype'],
+          'type' => row['type'],
+          'comments' => row['text'].split(/<hr \/>/).map {|comment| comment.gsub('&quot;', "\"").gsub('&lt;', "<").gsub('&gt;', ">").gsub('&amp;', "&")},
+        }
+        }
+        res_features << f
+      }
+    }
+
+    sleep(2)
+    @cgi.out {
+      JSON.generate result
+    }
+  end
+end
+
+bc = BugsController.new
+bc.dispatch!
+
