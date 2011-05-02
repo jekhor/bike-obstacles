2f338605734800a8faa5f2a42ce8442d66a2f8ef
Yauhen Kharuzhy <jekhor@gmail.com>
2011-05-02 20:52:32 +0300

    strategy-cluster-extended: Convert AttributeCluster to AttributesCluster strategy
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- js/strategy-cluster-extended.js
+++ js/strategy-cluster-extended.js
@@ -1,15 +1,15 @@
 /**
- * Class: OpenLayers.Strategy.AttributeCluster
+ * Class: OpenLayers.Strategy.AttributesCluster
  * Strategy for vector feature clustering based on feature attributes.
  *
  * Inherits from:
  *  - <OpenLayers.Strategy.Cluster>
  */
-OpenLayers.Strategy.AttributeCluster = OpenLayers.Class(OpenLayers.Strategy.Cluster, {
+OpenLayers.Strategy.AttributesCluster = OpenLayers.Class(OpenLayers.Strategy.Cluster, {
     /**
      * the attribute to use for comparison
      */
-    attribute: null,
+    attributes: [],
     /**
      * Method: shouldCluster
      * Determine whether to include a feature in a given cluster.
@@ -22,13 +22,46 @@ OpenLayers.Strategy.AttributeCluster = OpenLayers.Class(OpenLayers.Strategy.Clus
      * {Boolean} The feature should be included in the cluster.
      */
     shouldCluster: function(cluster, feature) {
-        var cc_attrval = cluster.cluster[0].attributes[this.attribute];
-        var fc_attrval = feature.attributes[this.attribute];
+        var cc_attributes = cluster.cluster[0].attributes;
+        var fc_attributes = feature.attributes;
+        var attr;
+
+        var equalFlag = true;
+        for (var i = 0; i < this.attributes.length; i++) {
+          attr = this.attributes[i];
+          equalFlag = (cc_attributes[attr] == fc_attributes[attr]);
+
+          if (!equalFlag)
+            break;
+        }
+
         var superProto = OpenLayers.Strategy.Cluster.prototype;
-        return cc_attrval === fc_attrval && 
+        return equalFlag &&
                superProto.shouldCluster.apply(this, arguments);
     },
-    CLASS_NAME: "OpenLayers.Strategy.AttributeCluster"
+
+    /**
+     * Method: createCluster
+     * Given a feature, create a cluster.
+     *
+     * Parameters:
+     * feature - {<OpenLayers.Feature.Vector>}
+     *
+     * Returns:
+     * {<OpenLayers.Feature.Vector>} A cluster.
+     */
+    createCluster: function(feature) {
+        var cluster = OpenLayers.Strategy.Cluster.prototype.createCluster.apply(this, arguments);
+
+        for (var i = 0; i < this.attributes.length; i++) {
+          var attr = this.attributes[i];
+          cluster.attributes[attr] = feature.attributes[attr];
+        }
+
+        return cluster;
+    },
+
+    CLASS_NAME: "OpenLayers.Strategy.AttributesCluster"
 });
 
 /**
